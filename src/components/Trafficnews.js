b95f66bb6150702fa3285520a9c5a2075f7f4c77
osman ismail <osman.ism@live.nl>
2022-05-03 15:27:02 +0200

    test

--- src/components/Trafficnews.js
+++ src/components/Trafficnews.js
@@ -9,7 +9,7 @@ export default function Trafficnews() {
 
   useEffect(() => {
    const fetchIt = async ()=>{
-    await fetch('https://newsapi.org/v2/everything?q=traffic&apiKey=a85abafdd40249398d40a7794a9506a1')
+    await fetch('/api/v2/everything?q=traffic&apiKey=a85abafdd40249398d40a7794a9506a1')
       .then(res => res.json())
       .then(data => {
         setArticles(data.articles)
--- src/components/Worldnews.js
+++ src/components/Worldnews.js
@@ -7,7 +7,7 @@ export default function Worldnews() {
 
   useEffect(() => {
    const fetchIt = async ()=>{
-    await fetch('https://newsapi.org/v2/everything?q=world&apiKey=a85abafdd40249398d40a7794a9506a1')
+    await fetch('/api/v2/everything?q=world&apiKey=a85abafdd40249398d40a7794a9506a1')
       .then(res => res.json())
       .then(data => {
         setArticles(data.articles)
