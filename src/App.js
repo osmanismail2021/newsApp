b6abb09df7699ef51b8266c4572d8645feecc712
osman ismail <osman.ism@live.nl>
2022-05-03 11:09:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -15,43 +15,14 @@ import { React, useState, useEffect } from 'react';
 
 
 
-
 export default function App() {
   
   const [articles, setArticles] = useState([])
-  const express = require('express');
-  const { createProxyMiddleware } = require('http-proxy-middleware');
-
-  // proxy middleware options
-/** @type {import('http-proxy-middleware/dist/types').Options} */
-const options = {
-  target: 'http://osmani.netlify.app', // target host
-  changeOrigin: true, // needed for virtual hosted sites
-  ws: true, // proxy websockets
-  pathRewrite: {
-    '^/api/old-path': '/api/new-path', // rewrite path
-    '^/api/remove/path': '/path', // remove base path
-  },
-  router: {
-    // when request.headers.host == 'dev.localhost:3000',
-    // override target 'http://www.example.org' to 'http://localhost:8000'
-    'dev.localhost:3000': 'http://localhost:8000',
-  },
-};
-
-// create the proxy (without context)
-const exampleProxy = createProxyMiddleware(options);
-
-// mount `exampleProxy` in web server
-const app = express();
-app.use('/api', exampleProxy);
-app.listen(3000);
-  
 
 
   useEffect(() => {
    const fetchIt = async ()=>{
-    await fetch('/v2/everything?q=news&apiKey=a85abafdd40249398d40a7794a9506a1')
+    await fetch('https://newsapi.org/v2/everything?q=news&apiKey=a85abafdd40249398d40a7794a9506a1')
       .then(res => res.json())
       .then(data => {
         setArticles(data.articles)
