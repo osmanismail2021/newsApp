016706043023039882f8fe5fc081f412eec13f29
osman ismail <osman.ism@live.nl>
2022-05-12 10:30:10 +0200

    update

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,10 +1,49 @@
 import React from 'react'
 import "./style/footer.css"
+import { Link } from "react-router-dom";
+import { BsCloudSun } from "react-icons/bs";
+import { GiBelgium } from "react-icons/gi";
+import { BiWorld } from "react-icons/bi";
+import { BiSearchAlt } from "react-icons/bi";
+import { MdOutlineSportsSoccer } from "react-icons/md"
+import { GiTrafficLightsReadyToGo } from "react-icons/gi"
+
 
 export default function Footer() {
   return (
     <div className='footer'>
-        <h1>Created by_Osman</h1>
+   <div className='itemsContainer'>
+        <Link className='icon' to="/belgiumnews">
+
+          <span>Belgium News</span>
+          <GiBelgium />
+
+        </Link>
+        <Link className='icon' to="/worldnews">
+
+          <span>World News</span>
+          <BiWorld />
+        </Link>
+        <Link className='icon' to="/weathernews">
+
+          <span>Weather News</span>
+          <BsCloudSun />
+
+        </Link>
+        <Link className='icon' to="/sportnews">
+
+          <span>Sport News</span>
+          <MdOutlineSportsSoccer />
+
+        </Link>
+        <Link className='icon' to="/trafficnews">
+
+          <span>Traffic News</span>
+          <GiTrafficLightsReadyToGo />
+
+        </Link>
+      </div>
+        <h1>copyright © 2022 design and developed by Osman ISMAIL</h1>
     </div>
   )
 }
